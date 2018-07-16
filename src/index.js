858d886543265a6bd6485231715c44eb63ff788b
Nishiki Liu <nshki@users.noreply.github.com>
2018-07-16 01:28:35 -0400

    Install React Apollo

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './reset.css.js';
 import MapScreen from './views/MapScreen';
 import registerServiceWorker from './registerServiceWorker';
 
+const apolloClient = new ApolloClient({ uri: process.env.REACT_APP_API_ENDPOINT });
+
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          <Route exact path="/" component={MapScreen} />
-        </Router>
+        <ApolloProvider client={apolloClient}>
+          <Router>
+            <Route exact path="/" component={MapScreen} />
+          </Router>
+        </ApolloProvider>
       </Provider>
     );
   }
--- src/views/MapScreen/index.js
+++ src/views/MapScreen/index.js
@@ -4,12 +4,11 @@ import MenuBar from '../../components/MenuBar';
 import Map from '../../components/Map';
 
 class MapScreen extends React.Component {
-  componentDidMount() {
-    const params = qs.parse(this.props.location.search.replace('?', ''));
-    console.log(params);
-  }
 
   render() {
+    const params = qs.parse(this.props.location.search.replace('?', ''));
+    console.log(params);
+
     return (
       <React.Fragment>
         <MenuBar />
