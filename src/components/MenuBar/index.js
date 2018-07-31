e8fa69802aa9d2169b1b961d053eb49241366f3f
Nishiki Liu <nshki@users.noreply.github.com>
2018-07-31 19:31:48 -0400

    Commit to app name

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -21,7 +21,7 @@ class MenuBar extends React.Component {
 
     return (
       <Container>
-        <Logo>Kacha</Logo>
+        <Logo>Play Local</Logo>
         {action}
       </Container>
     );
